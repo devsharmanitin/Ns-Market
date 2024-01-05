bbc75461de9012a4b4e47f535c12219f8837d940
Sharma Nitin <sharmanitin4749@gmail.com>
2024-01-05 11:56:45 +0530

    Update settings.py

--- NsMarkat/Shopx/Shopx/settings.py
+++ NsMarkat/Shopx/Shopx/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nssh4707@gmail.com'
-EMAIL_HOST_PASSWORD = 'najkpaanoxrhxpio'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 # Crediential_File_Path =  "C:\\Users\\DELL\\OneDrive\\Documents\\Crediential\\Crediential.txt"
 
